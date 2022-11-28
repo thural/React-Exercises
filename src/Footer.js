362c5a994d96c0d96b6f8b97af15b9b2d3fda321
Thural Musaibov <tural.musaibov@outlook.com>
2022-11-28 22:41:18 +0400

    split page into components

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+  return (
+      <footer>
+          <small>© 2021 Ziroll development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-
-const navbar = (
-  <nav>
-      <h1>Bob's Bistro</h1>
-      <ul>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-      </ul>
-  </nav>
-)
+import './index.css'
+import Page from "./Page"
 
 //ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
 
-ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />)
